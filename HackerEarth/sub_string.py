531dd04782c930b9fc8db462dcd7afea9fb9176a
kasule joseph <joseph.kasule@andela.com>
2019-11-20 15:19:27 +0200

    add generate sub string function

--- HackerEarth/sub_string.py
+++ HackerEarth/sub_string.py
@@ -4,6 +4,20 @@ s = str(input())
 s_array = list(s)
 sub_string = map(int, s_array[:k])
 reminder_String = map(int, s_array[k:])
+
+def generate_sub_strings():
+    start_range = stop_range = n
+    string_combination = []
+    while start_range > 0:
+        start_range -= k
+        print(start_range)
+        sub_string = map(int, [s_array[a] for a in range(start_range, stop_range)])
+        stop_range -= k
+        string_combination.append(sub_string)
+    return string_combination
+
+print(generate_sub_strings()[::-1],)
+
 sum_of_sub_string = sum(sub_string)
 count = 1
 reversed_sub = sub_string[::-1]
