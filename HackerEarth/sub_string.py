caa43c811cc8219b522808d4d3906f073ef21ca1
kasule joseph <joseph.kasule@andela.com>
2019-11-20 17:03:11 +0200

    add generate sub string function

--- HackerEarth/sub_string.py
+++ HackerEarth/sub_string.py
@@ -4,6 +4,8 @@ s = str(input())
 s_array = list(s)
 sub_string = map(int, s_array[:k])
 reminder_String = map(int, s_array[k:])
+only_transformed = []
+transformed_list = []
 
 def generate_sub_strings():
     start_range = stop_range = n
@@ -16,22 +18,42 @@ def generate_sub_strings():
         string_combination.append(sub_string)
     return string_combination
 
-print(generate_sub_strings()[::-1],)
+def transfrom_array(sum_of_sub_string, reversed_sub, only_transformed):
+    for i, value in reversed_sub.items():
+        print('i, x', i, value)
+        for x, y in enumerate(value):
+            print('x, y', x,y )
+            if(value[y] == 1):
+                while sum_of_sub_string > m:
+                    print(value)
+                    value[y] = 0
+                    transformed_list.insert(i, value)
+                    only_transformed.append(value)
+                    break
+            # print('reversed_sub', reversed_sub)
 
-sum_of_sub_string = sum(sub_string)
-count = 1
-reversed_sub = sub_string[::-1]
 
-for i, x in enumerate(reversed_sub):
-    if(reversed_sub[i] == 1):
-        while sum_of_sub_string > m:
-            count +=1
-            reversed_sub[i] = 0
-            sum_of_sub_string = sum(reversed_sub)
-            break
-transformed = reversed_sub[::-1] + reminder_String
-print(count)
-print(''.join(str(x) for x in transformed))
+
+
+# print(generate_sub_strings()[::-1])
+transformed = []
+reversed_sub = generate_sub_strings()[::-1]
+for i in range(len(reversed_sub)):
+    sum_of_sub_string = sum(reversed_sub[i])
+    dic_reversed_sub = {i: reversed_sub[i]}
+    transfrom_array(sum_of_sub_string, dic_reversed_sub, only_transformed)
+
+print('count', only_transformed)
+print('transformed_list', transformed_list)
+
+
+# sum_of_sub_string = sum(sub_string)
+# count = 1
+# reversed_sub = sub_string[::-1]
+
+# transformed = reversed_sub[::-1] + reminder_String
+# print(count)
+# print(''.join(str(x) for x in transformed))
 
 
 
