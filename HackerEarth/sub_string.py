12e7cb187679faad32f79a1772fc5c085130f678
kasule joseph <joseph.kasule@andela.com>
2019-11-22 14:46:40 +0200

    deliver sub string challenge

--- HackerEarth/sub_string.py
+++ HackerEarth/sub_string.py
@@ -1,9 +1,9 @@
+from time import time, sleep
+start_time = time()
 n, k , m = input()
 s = str(input())
 
 s_array = list(s)
-sub_string = map(int, s_array[:k])
-reminder_String = map(int, s_array[k:])
 only_transformed = []
 transformed_dict = dict()
 
@@ -12,42 +12,42 @@ def generate_sub_strings():
     string_combination = []
     while start_range > 0:
         start_range -= k
-        print(start_range)
         sub_string = map(int, [s_array[a] for a in range(start_range, stop_range)])
         stop_range -= k
         string_combination.append(sub_string)
     return string_combination
+    
+def update_ones_while_sum_lessthan_m(list_of_items, i):
+    list_of_items = list_of_items[0]
+    for key, item in enumerate(list_of_items):
+        if(list_of_items[item] == 1 and sum(list_of_items) > m):
+            list_of_items[item] = 0
+            transformed_dict[i] = list_of_items
+            only_transformed.append(list_of_items)
 
-def transfrom_array(sum_of_sub_string, reversed_sub, only_transformed):
-    yy = [value for i, value in reversed_sub.items()]
-    print('yy',yy , i)
-    if sum(yy[0]) > m:
-        for x, y in enumerate(yy[0]):
-            if(yy[0][y] == 1):
-                while sum(yy[0]) > m:
-                    # print('iiiiii', i)
-                    yy[0][y] = 0
-                    transformed_dict[i] = yy[0]
-                    only_transformed.append(yy[0])
-                    print('transformed_dict[i]', transformed_dict[i])
-                    if(i != i):
-                        print('iiiiii', i)
-                    break
+def transfrom_array(reversed_sub, only_transformed, i):
+    list_of_items= [value for i, value in reversed_sub.items()]
+    if sum(list_of_items[0]) > m:
+        update_ones_while_sum_lessthan_m(list_of_items, i)
     else:
-        transformed_dict[i] = yy[0]
-
+        transformed_dict[i] = list_of_items[0]
 
 
 # print(generate_sub_strings()[::-1])
-transformed = []
+
 reversed_sub = generate_sub_strings()[::-1]
 for i in range(len(reversed_sub)):
-    sum_of_sub_string = sum(reversed_sub[i])
     dic_reversed_sub = {i: reversed_sub[i]}
-    transfrom_array(sum_of_sub_string, dic_reversed_sub, only_transformed)
+    transfrom_array(dic_reversed_sub, only_transformed, i)
 
-yy = [value for x, values in transformed_dict.items() for value in values]
-print('count', only_transformed)
-print('transformed_list', ''.join(str(x) for x in yy))
+transformed_list = [value for x, values in transformed_dict.items() for value in values]
+count = len(only_transformed)
+transformed = ''.join(str(x) for x in transformed_list)
+end_time = time()
+
+print(count)
+print(transformed)
+print(end_time-start_time)
+
 
 
