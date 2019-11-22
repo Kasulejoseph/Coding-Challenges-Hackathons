fd5a2ebf5f6c3189e3aaa0f8b37e959524bee4bb
kasule joseph <joseph.kasule@andela.com>
2019-11-22 12:26:28 +0200

    make changes

--- HackerEarth/sub_string.py
+++ HackerEarth/sub_string.py
@@ -5,7 +5,7 @@ s_array = list(s)
 sub_string = map(int, s_array[:k])
 reminder_String = map(int, s_array[k:])
 only_transformed = []
-transformed_list = []
+transformed_dict = dict()
 
 def generate_sub_strings():
     start_range = stop_range = n
@@ -19,19 +19,22 @@ def generate_sub_strings():
     return string_combination
 
 def transfrom_array(sum_of_sub_string, reversed_sub, only_transformed):
-    for i, value in reversed_sub.items():
-        print('i, x', i, value)
-        for x, y in enumerate(value):
-            print('x, y', x,y )
-            if(value[y] == 1):
-                while sum_of_sub_string > m:
-                    print(value)
-                    value[y] = 0
-                    transformed_list.insert(i, value)
-                    only_transformed.append(value)
+    yy = [value for i, value in reversed_sub.items()]
+    print('yy',yy , i)
+    if sum(yy[0]) > m:
+        for x, y in enumerate(yy[0]):
+            if(yy[0][y] == 1):
+                while sum(yy[0]) > m:
+                    # print('iiiiii', i)
+                    yy[0][y] = 0
+                    transformed_dict[i] = yy[0]
+                    only_transformed.append(yy[0])
+                    print('transformed_dict[i]', transformed_dict[i])
+                    if(i != i):
+                        print('iiiiii', i)
                     break
-            # print('reversed_sub', reversed_sub)
-
+    else:
+        transformed_dict[i] = yy[0]
 
 
 
@@ -43,23 +46,8 @@ for i in range(len(reversed_sub)):
     dic_reversed_sub = {i: reversed_sub[i]}
     transfrom_array(sum_of_sub_string, dic_reversed_sub, only_transformed)
 
+yy = [value for x, values in transformed_dict.items() for value in values]
 print('count', only_transformed)
-print('transformed_list', transformed_list)
+print('transformed_list', ''.join(str(x) for x in yy))
 
-
-# sum_of_sub_string = sum(sub_string)
-# count = 1
-# reversed_sub = sub_string[::-1]
-
-# transformed = reversed_sub[::-1] + reminder_String
-# print(count)
-# print(''.join(str(x) for x in transformed))
-
-
-
-
-# if ():
-#     print('yes')
-
-# print(sub_String)
 
