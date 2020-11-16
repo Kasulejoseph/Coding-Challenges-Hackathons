e8189d8a3c2bd01a260e73104c5a5cd333582d38
Kasulejoseph <kasule06joseph@gmail.com>
2020-11-16 21:50:34 +0300

    Rectangle overlap

--- LeetCode/rectangle_overlap.py
+++ LeetCode/rectangle_overlap.py
@@ -1,16 +1,11 @@
 class Solution:
     def isRectangleOverlap(self, rec1, rec2) -> bool:
-        if rec2[0] <= rec1[0]:
-            x_difference = rec2[-2] - rec1[0]
-            y_difference = rec2[-1] - rec1[1]
-            print(x_difference, y_difference)
-            return x_difference > 0 and y_difference > 0
-        xt_difference = rec1[-2] - rec2[0]
-        yt_difference = rec1[-1] - rec2[1]
-        print(rec1[-2], rec2[0])
-        print(xt_difference, yt_difference)
-        if xt_difference > 0 and yt_difference > 0:
-            return True
-        return False
+        if rec1[0] == rec1[-2] or rec2[1] == rec2[-1]:
+            return False
+        if rec2[0] == rec2[-2] or rec1[1] == rec1[-1]:
+            return False
+        x_not_overlap = rec1[-2] <= rec2[0] or rec1[0] >= rec2[-2]
+        y_not_overlap = rec1[-1] <= rec2[1] or rec1[1] >= rec2[-1]
+        return not (x_not_overlap or y_not_overlap)
 
 print(Solution().isRectangleOverlap([-9,6,-3,10], [-8,-10,-5,-4]))
